45e7d36e3dc2f58e4c60d715e9221806ac0e339a
securims <anna.maria.parandiy@gmail.com>
2022-09-24 19:44:31 +0300

    fix inference testing

--- inference/inference.py
+++ inference/inference.py
@@ -9,6 +9,7 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import load_model
 
 import autokeras as ak
 
@@ -18,11 +19,19 @@ def train_model(x_train: np.ndarray, y_train: np.ndarray) -> ak.TextRegressor:
     # Feed the text regressor with training data.
     reg.fit(x_train, y_train, epochs=2)
     # Predict with the best model.
+
+    model = reg.export_model()
+
+    try:
+        model.save("model_autokeras", save_format="tf")
+    except Exception:
+        model.save("model_autokeras.h5")
+
     return reg
 
 
-def get_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    train_dataset = pd.read_csv('./test.csv',
+def get_data(path: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    train_dataset = pd.read_csv(path,
                                 na_values="?", comment='\t',
                                 sep=",", skipinitialspace=True)
 
@@ -36,20 +45,23 @@ def get_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
 def predict(model: ak.TextRegressor, x: np.ndarray) -> np.ndarray:
     # replace with real model
-    time.sleep(0.001)
+    # time.sleep(0.001)
     return model.predict(x)
 
 
-def run_inference(model: ak.TextRegressor, x_test: np.ndarray, batch_size: int = 2048) -> np.ndarray:
+def run_inference(x_test: np.ndarray, batch_size: int = 64) -> np.ndarray:
+    model = load_model("model_autokeras", custom_objects=ak.CUSTOM_OBJECTS)
+
     y_pred = []
-    for i in tqdm(range(0, x_test.shape[0], batch_size)):
-        x_batch = x_test[i: i + batch_size]
-        y_batch = predict(model, x_batch)
-        y_pred.append(y_batch)
+    for k in range(0, 100):
+        for i in range(0, x_test.shape[0], batch_size):
+            x_batch = x_test[i: i + batch_size]
+            y_batch = predict(model, x_batch)
+            y_pred.append(y_batch)
     return np.concatenate(y_pred)
 
 
-def run_inference_process_pool(model: ak.TextRegressor, x_test: np.ndarray, max_workers: int = 8) -> np.ndarray:
+def run_inference_process_pool(x_test: np.ndarray, max_workers: int = 16) -> np.ndarray:
     with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
         chunk_size = len(x_test) // max_workers
 
@@ -61,7 +73,7 @@ def run_inference_process_pool(model: ak.TextRegressor, x_test: np.ndarray, max_
         futures = []
         # submit chunks for inference
         for chunk in chunks:
-            future = executor.submit(run_inference, model=model, x_test=chunk)
+            future = executor.submit(run_inference, x_test=chunk)
             futures.append(future)
 
         # # wait for all futures
@@ -76,16 +88,17 @@ def run_inference_process_pool(model: ak.TextRegressor, x_test: np.ndarray, max_
 
 
 def main():
-    x_train, y_train, x_test = get_data()
-    model = train_model(x_train, y_train)
+    x_train, y_train, x_test = get_data('./test.csv')
+
+    train_model(x_train, y_train)
+
+    # s = time.monotonic()
+    # _ = run_inference_process_pool(x_test=x_test, max_workers=1)
+    # print(f"Inference one worker {time.monotonic() - s}")
 
     s = time.monotonic()
-    _ = run_inference(model=model, x_test=x_test)
-    print(f"Inference one worker {time.monotonic() - s}")
-
-    s = time.monotonic()
-    max_workers = 8
-    _ = run_inference_process_pool(model=model, x_test=x_test)
+    max_workers = 16
+    _ = run_inference_process_pool(x_test=x_test, max_workers=max_workers)
     print(f"Inference {max_workers} workers {time.monotonic() - s}")
 
 
